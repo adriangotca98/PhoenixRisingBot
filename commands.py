1580c960256917361b8e00894b3d7d93defd0441
Adrian Gotca <adrian.gotca@bolt.eu>
2022-08-26 03:13:37 +0300

    renamed commands

--- commands.py
+++ commands.py
@@ -18,139 +18,139 @@ async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
     print('------')
 
-@bot.command(name='i_sol')
+@bot.command(name='isolm')
 async def solInit(ctx):
     await main.sendInitMessage(ctx,'AETERNUM SOL','Sol')
 
-@bot.command(name='i_ignis')
+@bot.command(name='iignism')
 async def ignisInit(ctx):
     await main.sendInitMessage(ctx,'AETERNUM IGNIS','Ignis')
 
-@bot.command(name='i_dust')
+@bot.command(name='idustm')
 async def dustInit(ctx):
     await main.sendInitMessage(ctx,'FROM THE DUST','Dust')
 
-@bot.command(name='i_ashes')
+@bot.command(name='iashesm')
 async def ashesInit(ctx):
     await main.sendInitMessage(ctx,'FROM THE ASHES','Ashes')
 
-@bot.command(name='i_reborn')
+@bot.command(name='irebornm')
 async def RebornInit(ctx):
     await main.sendInitMessage(ctx,'PHOENIX REBORN','Reborn')
 
-@bot.command(name='i_risen')
+@bot.command(name='irisenm')
 async def RisenInit(ctx):
     await main.sendInitMessage(ctx,'PHOENIX RISEN','Risen')
 
-@bot.command(name='i_helios')
+@bot.command(name='iheliosm')
 async def heliosInit(ctx):
     await main.sendInitMessage(ctx,'PHOENIX HELIOS','Helios')
 
-@bot.command(name='i_nova')
+@bot.command(name='inovam')
 async def novaInit(ctx):
     await main.sendInitMessage(ctx,'PHOENIX NOVA','Nova')
 
-@bot.command(name='i_vulcan')
+@bot.command(name='ivulcanm')
 async def vulcanInit(ctx):
     await main.sendInitMessage(ctx,'PHOENIX VULCAN','Vulcan')
 
-@bot.command(name='i_nebula')
+@bot.command(name='inebulam')
 async def nebulaInit(ctx):
     await main.sendInitMessage(ctx,'PHOENIX NEBULA','Nebula')
 
-@bot.command(name='i_titan')
+@bot.command(name='ititanm')
 async def titanInit(ctx):
     await main.sendInitMessage(ctx,'PHOENIX TITAN','Titan')
 
-@bot.command(name='i_bootes')
+@bot.command(name='ibootesm')
 async def bootesInit(ctx):
     await main.sendInitMessage(ctx,'BOOTES VOID','Bootes')
 
-@bot.command(name='i_ice')
+@bot.command(name='iicem')
 async def iceInit(ctx):
     await main.sendInitMessage(ctx,'PHOENIX ICE','Ice')
 
-@bot.command(name='i_fire')
+@bot.command(name='ifirem')
 async def fireInit(ctx):
     await main.sendInitMessage(ctx,'PHOENIX FIRE','Fire')
 
-@bot.command(name='i_dragon')
+@bot.command(name='idragonm')
 async def dragonInit(ctx):
     await main.sendInitMessage(ctx,'PHOENIX DRAGON','Dragon')
 
-@bot.command(name='i_astra')
+@bot.command(name='iastram')
 async def australasiaInit(ctx):
     await main.sendInitMessage(ctx,'PHOENIX AUSTRALASIA','Astra')
 
-@bot.command(name='i_kraken')
+@bot.command(name='ikraken')
 async def krakenInit(ctx):
     await main.sendInitMessage(ctx,'PHOENIX KRAKEN','Kraken')
 
-@bot.command(name='m_fire')
+@bot.command(name='firem')
 async def firePlayers(ctx):
     await main.getPlayersResponse(ctx, main.rolesPerCrewAndColor['Fire'], "Phoenix Fire")
 
-@bot.command(name='m_sol')
+@bot.command(name='solm')
 async def solPlayers(ctx):
     await main.getPlayersResponse(ctx, main.rolesPerCrewAndColor['Sol'], 'Aeternum Sol')
 
-@bot.command(name='m_dust')
+@bot.command(name='dustm')
 async def dustPlayers(ctx):
     await main.getPlayersResponse(ctx, main.rolesPerCrewAndColor['Dust'], "From The Dust")
 
-@bot.command(name='m_risen')
+@bot.command(name='risenm')
 async def risenPlayers(ctx):
     await main.getPlayersResponse(ctx, main.rolesPerCrewAndColor['Risen'], "Phoenix Risen")
 
-@bot.command(name='m_dragon')
+@bot.command(name='dragonm')
 async def dragonPlayers(ctx):
     await main.getPlayersResponse(ctx, main.rolesPerCrewAndColor['Dragon'], "Phoenix Dragon")
 
-@bot.command(name='m_helios')
+@bot.command(name='heliosm')
 async def heliosPlayers(ctx):
     await main.getPlayersResponse(ctx, main.rolesPerCrewAndColor['Helios'], "Phoenix Helios")
 
-@bot.command(name='m_vulcan')
+@bot.command(name='vulcanm')
 async def vulcanPlayers(ctx):
     await main.getPlayersResponse(ctx, main.rolesPerCrewAndColor['Vulcan'], "Phoenix Vulcan")
 
-@bot.command(name='m_bootes')
+@bot.command(name='bootesm')
 async def bootesPlayers(ctx):
     await main.getPlayersResponse(ctx, main.rolesPerCrewAndColor['Bootes'], "Bootes Void")
 
-@bot.command(name='m_ice')
+@bot.command(name='icem')
 async def icePlayers(ctx):
     await main.getPlayersResponse(ctx, main.rolesPerCrewAndColor['Ice'], "Phoenix Ice")
 
-@bot.command(name='m_ignis')
+@bot.command(name='ignism')
 async def ignisPlayers(ctx):
     await main.getPlayersResponse(ctx, main.rolesPerCrewAndColor['Ignis'], "Aeternum Ignis")
 
-@bot.command(name='m_ashes')
+@bot.command(name='ashesm')
 async def ashesPlayers(ctx):
     await main.getPlayersResponse(ctx, main.rolesPerCrewAndColor['Ashes'], "From The Ashes")
 
-@bot.command(name='m_nebula')
+@bot.command(name='nebulam')
 async def nebulaPlayers(ctx):
     await main.getPlayersResponse(ctx, main.rolesPerCrewAndColor['Nebula'], "Phoenix Nebula")
 
-@bot.command(name='m_nova')
+@bot.command(name='novam')
 async def novaPlayers(ctx):
     await main.getPlayersResponse(ctx, main.rolesPerCrewAndColor['Nova'], "Phoenix Nova")
 
-@bot.command(name='m_reborn')
+@bot.command(name='rebornm')
 async def rebornPlayers(ctx):
     await main.getPlayersResponse(ctx, main.rolesPerCrewAndColor['Reborn'], "Phoenix Reborn")
 
-@bot.command(name='m_titan')
+@bot.command(name='titanm')
 async def titanPlayers(ctx):
     await main.getPlayersResponse(ctx, main.rolesPerCrewAndColor['Titan'], "Phoenix Titan")
 
-@bot.command(name='m_kraken')
+@bot.command(name='krakenm')
 async def krakenPlayers(ctx):
     await main.getPlayersResponse(ctx, main.rolesPerCrewAndColor['Kraken'], "Phoenix Kraken")
 
-@bot.command(name='m_astra')
+@bot.command(name='astram')
 async def australasiaPlayers(ctx):
     await main.getPlayersResponse(ctx, main.rolesPerCrewAndColor['AustralAsia'], "Phoenix AustralAsia")
 
