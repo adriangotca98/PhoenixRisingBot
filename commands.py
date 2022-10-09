b58bf281a3c9499fdc76ce8e3887f156aba4fedf
Adrian Gotca <adrian.gotca@bolt.eu>
2022-10-09 20:47:31 +0300

    added checks for guild_ids to prevent future problems

--- commands.py
+++ commands.py
@@ -30,7 +30,7 @@ async def on_application_command_error(ctx, error):
     await ctx.send_response(f"An unexpected error has occured. <@308561593858392065>, please have a look in the code. Command run: {ctx.command.name}")
     raise error
 
-@bot.slash_command(name="members", description="Used to get members of a certain crew")
+@bot.slash_command(name="members", description="Used to get members of a certain crew", guild_ids=[main.risingServerId, main.racingServerId, main.knowingServerId, main.serveringServerId])
 @commands.has_role("Phoenix Family Leadership")
 @discord.option(
     "crew_name",
@@ -42,7 +42,7 @@ async def getMembers(ctx: discord.ApplicationContext, crew_name: str):
     await main.getPlayersResponse(ctx, crew_name)
     await ctx.send_response("OK, all good!", ephemeral=True)
 
-@bot.slash_command(name='score', description='Set score for the crew in the CREW TABLES section and reorder the channels by score.')
+@bot.slash_command(name='score', description='Set score for the crew in the CREW TABLES section and reorder the channels by score.', guild_ids=[main.risingServerId, main.racingServerId, main.knowingServerId, main.serveringServerId])
 @commands.has_role("Phoenix Family Leadership")
 @discord.option(
     "crew_name",
@@ -60,7 +60,7 @@ async def setScoreForCrew(ctx: discord.ApplicationContext, crew_name: str, score
     await main.setScore(ctx, crew_name, scoreStr)
     await ctx.send_response("Score updated :)",ephemeral=True)
 
-@bot.slash_command(name='kick', description='Kick a member from all servers (Rising, Knowing, Racing, Servering).')
+@bot.slash_command(name='kick', description='Kick a member from all servers (Rising, Knowing, Racing, Servering).', guild_ids=[main.risingServerId, main.racingServerId, main.knowingServerId, main.serveringServerId])
 @commands.has_permissions(kick_members=True)
 @discord.option(
     "user",
@@ -72,7 +72,7 @@ async def kick(ctx: discord.ApplicationContext, user: discord.Member, reason: st
     await main.kickOrBanOrUnban(user, 'kick', bot, reason = reason)
     await ctx.send_response("User kicked :)",ephemeral=True)
 
-@bot.slash_command(name='ban',description='Ban a member from all servers (Rising, Knowing, Racing, Servering).')
+@bot.slash_command(name='ban',description='Ban a member from all servers (Rising, Knowing, Racing, Servering).', guild_ids=[main.risingServerId, main.racingServerId, main.knowingServerId, main.serveringServerId])
 @commands.has_permissions(ban_members=True)
 @discord.option(
     "user",
@@ -84,7 +84,7 @@ async def ban(ctx: discord.ApplicationContext, user: discord.Member, reason: str
     await main.kickOrBanOrUnban(user, 'ban', bot, reason=reason)
     await ctx.send_response("User banned :)", ephemeral=True)
 
-@bot.slash_command(name='unban', description='Unban a former member from all servers.')
+@bot.slash_command(name='unban', description='Unban a former member from all servers.', guild_ids=[main.risingServerId, main.racingServerId, main.knowingServerId, main.serveringServerId])
 @commands.has_permissions(ban_members=True)
 @discord.option(
     "user",
@@ -96,7 +96,7 @@ async def unban(ctx: discord.ApplicationContext, user: discord.Member):
     await main.kickOrBanOrUnban(user, 'unban', bot)
     await ctx.send_response("User unbanned :)", ephemeral=True)
 
-@bot.slash_command(name="multiple", description="Keep track of multiple accounts of the same person (same discord profile) within the same crew")
+@bot.slash_command(name="multiple", description="Keep track of multiple accounts of the same person (same discord profile) within the same crew", guild_ids=[main.risingServerId, main.racingServerId, main.knowingServerId, main.serveringServerId])
 @discord.option(
     "user",
     description='user that has multiple accounts in the crew (not multiple accounts in the family!)',
